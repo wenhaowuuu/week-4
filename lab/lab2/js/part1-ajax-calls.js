7e1f62321ce2c6466949a68485edde3c069ba576
jfrankl <jfrankl@azavea.com>
2017-02-03 12:16:41 -0500

    Initialize map

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -109,3 +109,20 @@ var phillyBikeCrashesDataUrl = "https://raw.githubusercontent.com/CPLN690-MUSA61
   Now that you've properly parsed your data, use _.each to plot the
   dataset you've pulled down.
 ===================== */
+
+
+/* =====================
+  Initialize map
+===================== */
+
+var map = L.map('map', {
+  center: [39.9522, -75.1639],
+  zoom: 14
+});
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
