09feddbf0c88c35478fcdcae2fc7cd145e9a514e
wenhaowuuu <wenhaowu92@gmail.com>
2017-07-11 21:32:01 -0400

    0711

--- lab/lab2/js/part1-ajax-calls.js
+++ lab/lab2/js/part1-ajax-calls.js
@@ -143,3 +143,31 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
   maxZoom: 20,
   ext: 'png'
 }).addTo(map);
+
+var map1 = L.map('map1', {
+  center: [39.9522, -75.1639],
+  zoom: 14
+});
+var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map1);
+
+console.log("test");
+
+var icon1 = L.icon({
+    iconUrl:'bike.png',
+    iconSize:[55,55],
+    iconAnchor:[22,94],
+    // popupAnchor:[-3,-76],
+    shadowUrl:'marker-shadow.png',
+    shadowSize:[48,65],
+    // shadowAnchor:[22,94]
+});
+
+L.circleMarker([39.9522, -75.1639]).addTo(map1);
+L.marker([39.9522, -75.269],{icon:icon1}).addTo(map1);
+console.log("marker");
